1ce84ae0d2c75d16b63ddf0a7e10c82111520136
Socheatha Tey <socheatha>
2022-01-14 22:50:41 +0700

    Clean code : user permission

--- app/Http/Controllers/Permission/PermissionController.php
+++ app/Http/Controllers/Permission/PermissionController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Permission;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Http\Requests\PermissionRequest;
 use App\Repositories\PermissionRepository;
 use Spatie\Permission\Models\Permission;
@@ -21,13 +20,10 @@ class PermissionController extends Controller
 	public function index()
 	{
 		$this->data = [
-				'permissions' => $this->permissions->getData(),
+			'permissions' => $this->permissions->getData(),
 		];
-
 		return view('permission.index', $this->data);
 	}
-
-
 
 	public function create()
 	{
@@ -36,56 +32,39 @@ class PermissionController extends Controller
 
 	public function store(PermissionRequest $request)
 	{
-
-		if ($this->permissions->create($request)){
-
-			// Redirect
+		if ($this->permissions->create($request)) {
 			return redirect()->route('permission.create')
 				->with('success', __('alert.crud.success.create', ['name' => Auth::user()->module()]) . $request->name);
 		}
 	}
 
-	public function show(Permissions $permissions)
-	{
-		//
-	}
-
 	public function edit(Permission $permission)
 	{
 		$this->data = [
-				'permission' => $permission,
+			'permission' => $permission,
 		];
-		
 		return view('permission.edit', $this->data);
 	}
 
 	public function update(PermissionRequest $request, $id)
 	{
-
-		if ($this->permissions->update($request, $id)){
-
-			// Redirect
+		if ($this->permissions->update($request, $id)) {
 			return redirect()->route('permission.index')
 				->with('success', __('alert.crud.success.update', ['name' => Auth::user()->module()]) . $request->name);
 		}
 	}
 
-
 	public function destroy($id)
 	{
-		// Redirect
 		return redirect()->route('permission.index')
 			->with('success', __('alert.crud.success.delete', ['name' => Auth::user()->module()]) . $this->permissions->destroy($id));
 	}
 
-	
 	public function permission_to_role()
 	{
 		$this->data = [
-				'permissions' => $this->permissions->getData(),
+			'permissions' => $this->permissions->getData(),
 		];
-
 		return view('permission.permission_to_role', $this->data);
 	}
-
 }
